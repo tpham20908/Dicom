24cd9b285720ba143e9c28bb81306baf7570a132
Vu Le <tpham20908@gmail.com>
2018-09-07 09:09:53 -0400

    deleted proj

--- S4STests-master/src/app/modules/Writer/helper.js
+++
@@ -1,8 +0,0 @@
-export async function WriteTitle(title, stream){
-	for(var i = 0; i < 25; i++)
-		stream.write("=")
-	stream.write(title);
-	for(var i = 0; i < 25; i++)
-		stream.write("=")
-	stream.write("\n");
-}
